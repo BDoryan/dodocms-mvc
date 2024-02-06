3fb75e7717b5511c0d512d036a59a9d38365856b
Doryqn <antox.doryan@gmail.com>
2024-02-07 00:07:10 +0100

    save changes

--- core/classes/DodoCMS.php
+++ core/classes/DodoCMS.php
@@ -5,7 +5,7 @@ class DodoCMS
 
     const AUTHOR = "Doryan BESSIERE";
     const CONTRIBUTORS = ["Baptiste BOUTEILLE"];
-    const VERSION = "02.06.02.2024";
-    const VERSION_TIMESTAMP = 1707256830;
+    const VERSION = "07.02.2024";
+    const VERSION_TIMESTAMP = 1707260810;
 
 }
