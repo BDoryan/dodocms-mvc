821a18a8b92b14d6d1d283a11b8507be484e16cb
Doryqn <antox.doryan@gmail.com>
2024-02-15 00:01:32 +0100

    [UPDATE] Change the name of models and login system

--- core/classes/DodoCMS.php
+++ core/classes/DodoCMS.php
@@ -5,7 +5,7 @@ class DodoCMS
 
     const AUTHOR = "Doryan BESSIERE";
     const CONTRIBUTORS = ["Baptiste BOUTEILLE"];
-    const VERSION = "development-9-12.02.2024";
+    const VERSION = "15.02.2024";
     const VERSION_TIMESTAMP = 1707777712;
 
 }
