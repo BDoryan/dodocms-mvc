b9acc4a1b654ef00fad8e6defd822d6179dc8283
Doryqn <antox.doryan@gmail.com>
2024-02-16 00:30:04 +0100

    Change logs update test

--- core/classes/DodoCMS.php
+++ core/classes/DodoCMS.php
@@ -5,7 +5,7 @@ class DodoCMS
 
     const AUTHOR = "Doryan BESSIERE";
     const CONTRIBUTORS = ["Baptiste BOUTEILLE"];
-    const VERSION = "3-16.02.2024";
+    const VERSION = "4-16.02.2024";
     const VERSION_TIMESTAMP = 1707956256;
 
 }
