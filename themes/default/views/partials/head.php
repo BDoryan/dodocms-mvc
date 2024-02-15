416aabc84c06dea9ab05624ee3fc1e445a3f1ae8
Doryqn <antox.doryan@gmail.com>
2024-02-15 01:26:34 +0100

    Add scritps.php to theme default

--- themes/default/views/partials/head.php
+++ themes/default/views/partials/head.php
@@ -14,9 +14,3 @@
 
 <!-- Load dodocms styles -->
 <link rel="stylesheet" href="<?= Application::get()->toURL("/core/assets/css/dodocms.css") ?>">
-
-<!-- Load bootstrap scripts -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-
-<!-- Scripts -->
-<script src="<?= Application::get()->getTheme()->toURL('/assets/js/main.js') ?>"></script>
