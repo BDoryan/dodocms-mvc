e9a1dc8a106adea3ac16b1056efe46649bd6f46b
Doryqn <antox.doryan@gmail.com>
2024-02-19 23:44:52 +0100

    Add a warning when you leave without save your changes

--- core/classes/DodoCMS.php
+++ core/classes/DodoCMS.php
@@ -5,7 +5,7 @@ class DodoCMS
 
     const AUTHOR = "Doryan BESSIERE";
     const CONTRIBUTORS = ["Baptiste BOUTEILLE"];
-    const VERSION = "18.02.2024";
+    const VERSION = "development-18.02.2024";
     const VERSION_TIMESTAMP = 1707956256;
 
 }
