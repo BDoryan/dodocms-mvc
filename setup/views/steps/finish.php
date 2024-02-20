d3e699b728ddb5948fabe8b707c958276668167c
Doryqn <antox.doryan@gmail.com>
2024-02-20 19:01:16 +0100

    Setup update

--- setup/views/steps/finish.php
+++ setup/views/steps/finish.php
@@ -1,1 +1,5 @@
-<p class="text-center text-base text-green-700 font-semibold pb-5"><?= __('setup.finish.message') ?></p>
+<p class="tw-text-base tw-text-center tw-pb-5"><?= __('setup.finish.message') ?></p>
+<p class="tw-font-semibold tw-text-red-700"><?= __('setup.finish.message.delete_install') ?></p>
+<pre class="tw-w-full tw-overflow-auto tw-bg-gray-200 tw-p-2">
+<?= trim($_SERVER['DOCUMENT_ROOT'].'/setup') ?>
+</pre>
