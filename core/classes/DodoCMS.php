763a2fd55c5423434164bbd74e444bc2c23460c3
Doryqn <antox.doryan@gmail.com>
2024-06-03 19:00:27 +0200

    Change the update.php file

--- core/classes/DodoCMS.php
+++ core/classes/DodoCMS.php
@@ -5,7 +5,7 @@ class DodoCMS
 
     const AUTHOR = "Doryan BESSIERE";
     const CONTRIBUTORS = [""];
-    const VERSION = "05-3.06.2024";
+    const VERSION = "06-3.06.2024";
     const VERSION_TIMESTAMP = 1717431965;
 
 }
