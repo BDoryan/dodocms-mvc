da48e90d3dbf404ec5234cb25a698a3724002191
Doryqn <antox.doryan@gmail.com>
2024-06-03 18:26:18 +0200

    Add dashboard button on back office

--- core/classes/DodoCMS.php
+++ core/classes/DodoCMS.php
@@ -6,6 +6,6 @@ class DodoCMS
     const AUTHOR = "Doryan BESSIERE";
     const CONTRIBUTORS = [""];
     const VERSION = "02-3.06.2024";
-    const VERSION_TIMESTAMP = 1709642113;
+    const VERSION_TIMESTAMP = 1717431965;
 
 }
